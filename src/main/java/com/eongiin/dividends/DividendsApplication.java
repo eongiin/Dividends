0972778a69433c00d5b543e6618d422f6a8319dd
eongiin <eongiin@gmail.com>
2022-08-01 15:02:40 +0900

    Initial commit

---
+++ src/main/java/com/eongiin/dividends/DividendsApplication.java
@@ -0,0 +1,12 @@
+package com.eongiin.dividends;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DividendsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DividendsApplication.class, args);
+	}
+}
