6226c975d2b34acdc703ddf7ad64eaf84f9cc115
eongiin <eongiin@gmail.com>
2022-08-06 17:47:54 +0900

    feat: 배당금 조회

--- src/main/java/com/eongiin/dividends/persist/DividendRepository.java
+++ src/main/java/com/eongiin/dividends/persist/DividendRepository.java
@@ -5,6 +5,9 @@ import com.eongiin.dividends.persist.entity.DividendEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface DividendRepository extends JpaRepository<DividendEntity,Long> {
+public interface DividendRepository extends JpaRepository<DividendEntity, Long> {
+    List<DividendEntity> findAllByCompanyId(Long id);
 }
--- src/main/java/com/eongiin/dividends/web/FinanceController.java
+++ src/main/java/com/eongiin/dividends/web/FinanceController.java
@@ -1,5 +1,7 @@
 package com.eongiin.dividends.web;
 
+import com.eongiin.dividends.service.FinanceService;
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,9 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/finance")
+@AllArgsConstructor
 public class FinanceController {
+    private final FinanceService financeService;
+
     @GetMapping("/dividend/{companyName}")
     public ResponseEntity<?> searchFinance(@PathVariable String companyName) {
-        return null;
+        return ResponseEntity.ok(this.financeService.getDividendByCompanyName(companyName));
     }
 }
