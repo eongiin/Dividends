ae61fcd4dd1f8dc2ebe10217e459f59b8ba095b8
eongiin <eongiin@gmail.com>
2022-08-07 15:59:28 +0900

    feat: 스케쥴러

---
+++ src/main/java/com/eongiin/dividends/scheduler/ScraperScheduler.java
@@ -0,0 +1,57 @@
+package com.eongiin.dividends.scheduler;
+
+import com.eongiin.dividends.model.Company;
+import com.eongiin.dividends.model.ScrapedResult;
+import com.eongiin.dividends.persist.CompanyRepository;
+import com.eongiin.dividends.persist.DividendRepository;
+import com.eongiin.dividends.persist.entity.CompanyEntity;
+import com.eongiin.dividends.persist.entity.DividendEntity;
+import com.eongiin.dividends.scraper.Scraper;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Slf4j
+@Component
+@AllArgsConstructor
+public class ScraperScheduler {
+    private final CompanyRepository companyRepository;
+    private final DividendRepository dividendRepository;
+    private final Scraper yahooFinanceScraper;
+
+    @Scheduled(cron = "${scheduler.scrap.yahoo}")
+    public void yahooFinanceScheduling() {
+
+        log.info("scraping scheduler is started");
+        // 저장된 회사 목록 조회
+        List<CompanyEntity> companies = this.companyRepository.findAll();
+
+        // 회사마다 배당금 정보를 새로 스크래핑
+        for (CompanyEntity company : companies) {
+            log.info("scraping scheduler is started -> " + company.getName());
+            ScrapedResult scrapedResult = this.yahooFinanceScraper.scrap(Company.builder()
+                    .name(company.getName())
+                    .ticker(company.getTicker())
+                    .build());
+
+            // 스크래핑한 배당금 정보 중 데이터베이스에 없는 값은 저장
+            scrapedResult.getDividends().stream()
+                    .map(e -> new DividendEntity(company.getId(), e))
+                    .forEach(e -> {
+                        boolean exists = this.dividendRepository.existsByCompanyIdAndDate(e.getCompanyId(), e.getDate());
+                        if (!exists) {
+                            this.dividendRepository.save(e);
+                        }
+                    });
+            // 연속적으로 스크래핑 대상 사이트 서버에 요청을 날리지 않도록 일시 정지
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+}
